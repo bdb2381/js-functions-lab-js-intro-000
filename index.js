e81a3bb4294f2cb52298b3bf4a5e342a58ce20e0
Bradley Beggs <brad.beggs@gmail.com>
2020-06-09 03:16:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function happyHolidays(){
+function happyHolidays(){       //tell user Happy Holiday
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(Justine){
+function happyHolidaysTo(Justine){    //give a custom greating
   return `Happy holidays, ${Justine}!`
 }
 
-function happyCustomHolidayTo(holiday=" Day", name="Justine"){
+function happyCustomHolidayTo(holiday=" Day", name="Justine"){  //give a custom greeting for any holiday
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown (holiday="Mother\'s", days=20){
+function holidayCountdown (holiday="Mother\'s", days=20){       //count down the days till a holiday
   return `It\'s ${days} days until ${holiday}!`
 }
