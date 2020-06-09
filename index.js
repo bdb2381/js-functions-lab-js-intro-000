a9946677e277926a2d4c1428e3c06507ea16406c
Bradley Beggs <brad.beggs@gmail.com>
2020-06-09 03:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function happyHolidaysTo(Justine){
 function happyCustomHolidayTo(holiday=" Day", name="Justine"){
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown (holiday="Yay", days=100){
+  return `It's ${days} days until ${holiday}`
+}
